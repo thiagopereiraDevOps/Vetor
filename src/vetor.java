998218286781f6328da8eb5a81e7be53e623d967
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-03-09 16:11:41 -0300

    Meus arquivos de vetor

---
+++ src/vetor.java
@@ -0,0 +1,11 @@
+public class vetor {
+    public static void main(String[] args) {
+        int f[] = new int[10];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < 10; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+            System.out.println("Na posicao "+ i + ", temos o valor: " + f[i]);
+        }
+    }
+}
